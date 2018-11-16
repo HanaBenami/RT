7866dc6320952f7e635555e7954527c32dfb6528
hanabenami <hana.benami@gmail.com>
2018-11-16 15:42:10 +0200

    heroku

--- src/main/java/il/co/rtcohen/rt/Application.java
+++ src/main/java/il/co/rtcohen/rt/Application.java
@@ -7,10 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import static eu.hurion.vaadin.heroku.VaadinForHeroku.forApplication;
 import static eu.hurion.vaadin.heroku.VaadinForHeroku.herokuServer;
 
-@SpringBootApplication()
+//@SpringBootApplication()
 public class Application {
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+//		SpringApplication.run(Application.class, args);
 		herokuServer(forApplication(MainUI.class)).start();
 /* https://rt2019.herokuapp.com/
  git push heroku master*/
