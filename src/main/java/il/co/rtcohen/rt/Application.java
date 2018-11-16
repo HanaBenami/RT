27d794878acaeae7393303984f174c1f6d359881
hanabenami <hana.benami@gmail.com>
2018-11-16 15:20:12 +0200

    16/11/18 commit

--- src/main/java/il/co/rtcohen/rt/Application.java
+++ src/main/java/il/co/rtcohen/rt/Application.java
@@ -1,12 +1,18 @@
 package il.co.rtcohen.rt;
 
+import il.co.rtcohen.rt.app.MainUI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static eu.hurion.vaadin.heroku.VaadinForHeroku.forApplication;
+import static eu.hurion.vaadin.heroku.VaadinForHeroku.herokuServer;
 
 @SpringBootApplication()
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-    }
+		//herokuServer(forApplication(MainUI.class)).start();
+
+	}
 
 }
