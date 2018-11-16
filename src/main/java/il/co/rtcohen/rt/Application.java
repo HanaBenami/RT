aef52efccd7ce8da4dbf6b1c4dcbf1ba0bb7edfd
hanabenami <hana.benami@gmail.com>
2018-11-16 14:00:55 +0200

    16/11/18 commit

--- src/main/java/il/co/rtcohen/rt/Application.java
+++ src/main/java/il/co/rtcohen/rt/Application.java
@@ -1,17 +1,17 @@
 package il.co.rtcohen.rt;
 
-import il.co.rtcohen.rt.app.MainUI;
+//import il.co.rtcohen.rt.app.MainUI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import static eu.hurion.vaadin.heroku.VaadinForHeroku.forApplication;
-import static eu.hurion.vaadin.heroku.VaadinForHeroku.herokuServer;
+//import static eu.hurion.vaadin.heroku.VaadinForHeroku.forApplication;
+//import static eu.hurion.vaadin.heroku.VaadinForHeroku.herokuServer;
 
 @SpringBootApplication()
 public class Application {
 	public static void main(String[] args) {
-		//SpringApplication.run(Application.class, args);
-        herokuServer(forApplication(MainUI.class)).start();
+		SpringApplication.run(Application.class, args);
+        //herokuServer(forApplication(il.co.rtcohen.rt.app.MainUI.class)).start();
     }
 /* https://rt2019.herokuapp.com/
  git push heroku master*/
